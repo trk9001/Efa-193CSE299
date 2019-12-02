a16f251c42dcd0d9059ae9709ef62e2f91b34a51
trk <dev.trk.9001@gmail.com>
2019-12-03 05:25:52 +0600

    Update code to ES6 and put JS into a handler

--- static/script.js
+++ static/script.js
@@ -1,10 +1,10 @@
 function setCurrentTime() {
-    now = new Date();
-    time_text = now.toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric'});
-    date_text = now.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'});
+    let now = new Date();
+    let time_text = now.toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric'});
+    let date_text = now.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'});
     $('#current-time-text').text(time_text);
     $('#current-date-text').text(date_text);
-    current_hour = now.getHours();
+    let current_hour = now.getHours();
     if (current_hour >= 6 && current_hour < 18) {
         $('#current-time-icon').removeClass('fa-moon').addClass('fa-sun');
     } else {
@@ -12,5 +12,7 @@ function setCurrentTime() {
     }
 }
 
-setInterval(setCurrentTime, 1000);
-setCurrentTime();
+$(function () {
+    setInterval(setCurrentTime, 1000);
+    setCurrentTime();
+});
