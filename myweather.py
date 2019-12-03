c9c67857bd2cc80f3fcff8d84fab75dfc5e5bb77
trk9001 <dev.trk.9001@gmail.com>
2019-12-03 19:15:59 +0600

    Write a function to return past weather data

--- myweather.py
+++ myweather.py
@@ -1,3 +1,6 @@
+import datetime as dt
+from typing import List, Dict
+
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
@@ -18,7 +21,53 @@ class WeatherInfo(db.Model):
     temperature = db.Column(db.String(50))
 
     def __repr__(self):
-        return '<WeatherInfo ({})>'.format(self.time)
+        return '<WeatherInfo ({} {})>'.format(self.date, self.time)
+
+
+def past_n_days_weather(n) -> List[Dict[str, str]]:
+    """Return averages of weather data for the past n days."""
+    today_date = dt.date.today()
+    past_n_days = [today_date - dt.timedelta(days=i) for i in range(1, n + 1)]
+
+    # Query for the averages of stored weather data.
+    weather_data_averages = (
+        db.session.query(
+            db.func.avg(WeatherInfo.humidity),
+            db.func.avg(WeatherInfo.pressure_hg),
+            db.func.avg(WeatherInfo.pressure_mb),
+            db.func.avg(WeatherInfo.rain),
+            db.func.avg(WeatherInfo.temperature),
+        ).group_by(WeatherInfo.date)
+    )
+
+    info = []
+    for date in past_n_days:
+        # Retrieve the average weather data for a particular date.
+        weather_info = (
+            weather_data_averages
+            .filter(WeatherInfo.date == date)
+            .first()
+        )
+        if weather_info:  # if the data existed in the database
+            info.append({
+                'day': date.strftime('%A'),
+                'humidity': f'{weather_info[0]:.1f}',
+                'pressure_hg': f'{weather_info[1]:.1f}',
+                'pressure_mb': f'{weather_info[2]:.1f}',
+                'rain': f'{weather_info[3]:.1f}',
+                'temperature': f'{weather_info[4]:.1f}',
+            })
+        else:  # if the data did not exist
+            info.append({
+                'day': date.strftime('%A'),
+                'humidity': '-',
+                'pressure_hg': '-',
+                'pressure_mb': '-',
+                'rain': '-',
+                'temperature': '-',
+            })
+
+    return info
 
 
 @app.route('/')
